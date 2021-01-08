c71f6519bd299fa5dcbad8361ff2102b11ea8c74
Laura Maciel <laura.neves2008@hotmail.com>
2021-01-08 17:21:58 -0300

    Add username page

--- server.js
+++ server.js
@@ -3,12 +3,35 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app); // create a server object
 const io = require('socket.io')(http);
+const alert = require('alert')
 
-app.use('/public', express.static('public')); //using css template
+app.use('/views', express.static('views')); //using css template
+app.use(express.urlencoded({ extendend: false })); // related to post method
+app.set('view-engine', 'ejs')
+
+var users = [];
 
 // load main page
 app.get('/', (req, res)=>{
-  res.sendFile(__dirname + '/index.html');
+  res.render('index.ejs');
+})
+
+app.post('/', (req,res) => {
+  username = req.body.username;
+  if(users.includes(username)){
+    alert('Username already exists');
+    res.redirect('/');
+  }
+  else{
+    users.push(username);
+    res.redirect('/chat');
+  }
+  console.log(username);
+})
+
+app.get('/chat', (req, res) =>{
+  res.render('chat.ejs', {name:username})
+
 })
 
 // user connection
